76446c023c28af20b9d5c83a773fb17e84847593
Charles <46288486+ItzFrostbiteTv@users.noreply.github.com>
2019-02-12 19:39:18 -0600

    Add files via upload

--- Day 3 - Length Of Word.py
+++ Day 3 - Length Of Word.py
@@ -1,8 +1,67 @@
-print("Enter word to get length")
+print("Enter word to get length:")
 word = input()
 
 lengthOfWord = (len(word))
 print(lengthOfWord, "Letters")
 
-print("Press Anything To Continue")
-press = input()
+print("Would you like to go again [Y/N]")
+again = input()
+
+if again == "Y":
+    print("Enter word to get length: ")
+    word = input()
+    lengthOfWord = (len(word))
+    print(lengthOfWord, "Letters")
+    if again == "N":
+        print("Ok bye...")
+        bye = input()
+        quit()
+    if again == "n":
+        print("Ok bye...")
+        bye = input()
+        quit()
+
+#Lower Case y and n start
+
+if again == "y":
+    print("Enter word to get length: ")
+    word = input()
+    lengthOfWord = (len(word))
+    print(lengthOfWord, "Letters")
+    if again == "N":
+        print("Ok bye...")
+        bye = input()
+        quit()
+    if again == "n":
+        print("Ok bye...")
+        bye = input()
+        quit()
+
+
+if again == "n":
+    print("Ok bye...")
+    bye = input()
+    quit()
+
+#Lower Case y and n end
+
+if again == "N":
+    print("Ok bye...")
+    bye = input()
+    quit()
+
+while again != "N":
+    print("Would you like to go again [Y/N]")
+    again = input()
+    if again == "N":
+        print("Ok bye...")
+        bye = input()
+        quit()
+    if again == "n":
+        print("Ok bye...")
+        bye = input()
+        quit()
+    print("Enter word to get length: ")
+    word = input()
+    lengthOfWord = (len(word))
+    print(lengthOfWord, "Letters")
